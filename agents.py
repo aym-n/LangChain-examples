3a880ad57d8ea112630c6fec263c06a1eb5542be
aym-n <ajm8982@gmail.com>
2024-01-15 19:26:48 +0530

    tried adding chat history

--- agents.py
+++ agents.py
@@ -9,17 +9,28 @@ from langchain.agents.agent_types import AgentType
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langchain_community.utilities.sql_database import SQLDatabase
 
+from langchain_core.messages import AIMessage, HumanMessage
+
 db = SQLDatabase.from_uri("sqlite:///chinook.db")
+suffix = """Make sure to provide all the responses in markdown format
+current conversation:
+{chat_history}
+Begin!
+
+Question: {input}
+Thought: I should look at the tables in the database to see what I can query.  Then I should query the schema of the most relevant tables.
+{agent_scratchpad}"""
 
 agent_executor = create_sql_agent(
     llm=GoogleGenerativeAI(google_api_key=GOOGLE_API_KEY, model="models/text-bison-001"),
     toolkit=SQLDatabaseToolkit(db = db, llm=GoogleGenerativeAI(google_api_key=GOOGLE_API_KEY, model="models/text-bison-001")),
     verbose=True,
     agent_type= AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    suffix=suffix,
 )
 
 import streamlit as st
-
+chat_history = []
 st.title("Chinook Database Q&A")
 
 if "messages" not in st.session_state:
@@ -31,14 +42,16 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    chat_history.append(HumanMessage(content=prompt))
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    with st.chat_message("assistant"):
+    with st.chat_message("ai"):
         message_placeholder = st.empty()
-        message = agent_executor.invoke(prompt)
-        message_placeholder.write(message['output'])
-
+        message = agent_executor.invoke({"input": prompt, "chat_history": chat_history})
+        message_placeholder.markdown(message["output"])
     st.session_state.messages.append({"role": "assistant", "content": message['output']})
+    chat_history.append(AIMessage(content=message['output']))  
+
 
 
